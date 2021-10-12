c27669e1a2ddeab062596269cb38334c2fb5a72d
iQs Solution <32163698+definaru@users.noreply.github.com>
2021-10-12 20:57:36 +0300

    Update Pagination.php

--- Pagination.php
+++ Pagination.php
@@ -15,47 +15,76 @@ use yii\helpers\Html;
 class Pagination 
 {
     
+	
+    protected function viewPagination()
+    {
+        return YorClass::find();
+    } 
+	
     public function First() 
     {
 	// получаем id первой записи, в порядке убывания (1,2,3 ...40)
-        $model = YorClass::find()->orderBy(['id' => SORT_ASC])->limit(1)->one();
+        $model = self::viewPagination()
+		->orderBy(['id' => SORT_ASC])
+		->limit(1)
+		->one();
         return $model->id;
     }
     
     public function countPag($params = '') 
     {
 	// считаем количество записей до текущего id (по get параметру на текущей странице)
-        return YorClass::find()->where(['between', 'id', self::First(), $params])->count();
+        return self::viewPagination()
+		->where(['between', 'id', self::First(), $params])
+		->count();
     } 
     
     public function getNext($params = '')
     {
 	// узнаём id после текущего id
-        $model = YorClass::find()->select('id')->offset(self::countPag($params))->limit(self::First())->orderBy(['id' => SORT_ASC])->one();
+	$offset = self::countPag($params);
+	$limit = self::First()
+        $model = self::viewPagination()
+		->select('id')
+		->offset($offset)
+		->limit($limit)
+		->orderBy(['id' => SORT_ASC])
+		->one();
         return $model->id;
     }
     
     public function getPrev($params = '')
     {
 	// узнаём id перед текущим id
-        $model = YorClass::find()->select('id')->offset(self::countPag($params)-2)->limit(self::First())->orderBy(['id' => SORT_ASC])->one();
+	$offset = self::countPag($params)-2;
+	$limit = self::First();
+        $model = self::viewPagination()
+		->select('id')
+		->offset($offset)
+		->limit($limit)
+		->orderBy(['id' => SORT_ASC])
+		->one();
         return $model->id;
     }
 
-    public function getNextButton($params = '')
+    public function getNextButton($params = '', $icons = 'ionicons ion-arrow-right-c')
     {
 	// Данная функия по id определяет какую ссылку вывести, get параметр выводится по столбцу link
 	// функция выводит кнопку к следующей записи
-        $model = YorClass::find()->where(['id' => self::getNext($params)])->one();
-        return ($model == null) ? '' : Html::a('<i class="ionicons ion-arrow-right-c"></i>', '/blog/'.$model->link);
+	$next = self::getNext($params)
+        $model = self::viewPagination()->where(['id' => $next])->one();
+	$arrow_right = Html::tag('i', '', ['class' => $icons]);
+        return ($model == null) ? '' : Html::a($arrow_right, '/blog/'.$model->link);
     }
 
-    public function getPrevButton($params = '')
+    public function getPrevButton($params = '', $icons = 'ionicons ion-arrow-left-c')
     {
 	// Данная функия по id определяет какую ссылку вывести, get параметр выводится по столбцу link
 	// функция выводит кнопку к предыдущей записи
-        $madel = YorClass::find()->where(['id' => self::getPrev($params)])->one();
-        return ($madel->id == $params) ? '' : Html::a('<i class="ionicons ion-arrow-left-c"></i>', '/blog/'.$madel->link);
+	$prev = self::getPrev($params);
+        $model = self::viewPagination()->where(['id' => $prev])->one();
+	$arrow_left = Html::tag('i', '', ['class' => $icons]);  
+        return ($model->id == $params) ? '' : Html::a($arrow_left, '/blog/'.$model->link);
     }
 
     public function getPanel($params = '')
